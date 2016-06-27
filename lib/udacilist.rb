07be8d7e0245c5b40d76ea80f17e30e8a76d6e18
Kelly Howard <kelly.howard@knowlabs.com>
2016-06-27 13:42:18 -0700

    added error classes for 3 errors in udacilist add and delete methods

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -7,12 +7,24 @@ class UdaciList
     end
     def add(type, description, options={})
         type = type.downcase
-        @items.push TodoItem.new(description, options) if type == "todo"
-        @items.push EventItem.new(description, options) if type == "event"
-        @items.push LinkItem.new(description, options) if type == "link"
+        if !options[:priority] || options[:priority] =~ /\A(?:low|medium|high|)\z/
+            if type =~ /\A(?:todo|event|link)\z/
+                @items.push TodoItem.new(description, options) if type == "todo"
+                @items.push EventItem.new(description, options) if type == "event"
+                @items.push LinkItem.new(description, options) if type == "link"
+            else
+                raise UdaciListErrors::InvalidItemType, "That item type is invalid!"
+            end
+        else
+            raise UdaciListErrors::InvalidPriorityValue, "That priority is not an option!"
+        end
     end
     def delete(index)
-        @items.delete_at(index - 1)
+        if @items.length < index
+            raise UdaciListErrors::IndexExceedsListSize, "That item does not exist!"
+        else
+            @items.delete_at(index - 1)
+        end
     end
     def all
         puts "-" * @title.length
